b7c23afc63c4db4cf685094a51b1f6dcd6aeaea9
juanjo-lv <juanjoselopezvega94@gmail.com>
2021-04-09 20:27:42 +0200

    Update A2While1.py

--- Ejercicios_Control_Flujo/while/Ejercicio1/A2While1.py
+++ Ejercicios_Control_Flujo/while/Ejercicio1/A2While1.py
@@ -1,4 +1,8 @@
 mod = ""
 while mod == "":
-    mod = input("Teclee algun valor distinto a enter: ")
-print("has ingresado")
+    si_teclean = input("Teclee algun valor distinto a enter: ")
+    print("Valor booleano ",bool(si_teclean))
+    if si_teclean:
+        mod ="1"
+    else:
+        print("Por favor tecleen")
