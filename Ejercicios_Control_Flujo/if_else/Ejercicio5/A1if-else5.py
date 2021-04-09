717a14c5662006a2944da3f9858b486d96082d1b
juanjo-lv <juanjoselopezvega94@gmail.com>
2021-04-09 20:13:26 +0200

    asdasdasd

--- Ejercicios_Control_Flujo/if_else/Ejercicio5/A1if-else5.py
+++ Ejercicios_Control_Flujo/if_else/Ejercicio5/A1if-else5.py
@@ -5,32 +5,25 @@ if (num1>num2):
         print("Es mayor el ",num1)
         if (num2 > num3):
             print(" Es menor el ",num3)
-        elif num2 == num3:
-            print("2 y 3 son iguales")
         else:
             print(" Es menor el ",num2)
     else:
         if (num2 > num3):
             print("El mayor el ",num1)
             print("El menor el ",num3)
-        elif num2 == num3:
-            print("2 y 3 son iguales")    
+          
         else:
             print("El mayor el ",num3)
             print("El menor el ",num2)
-elif num1 == num2:
-    print(" 1 y 2 son iguales")
+elif (num1 == num2) (and num2 == num3):
+    print("Los numeros son iguales")
 else:
     if (num3 > num1):
         print(" Es menor el ",num1)
         if (num2 > num3):
-            print("El mayor el ",num2)
-        elif num2 == num3:
-            print("2 y 3 son iguales")   
+            print("El mayor el ",num2)  
         else:
-            print("El menor el ",num3)
-    elif num3 == num1:
-        print("3 y 1 son iguales")        
+            print("El menor el ",num3)       
     else:
         print(" Es mayor el ",num2)
         print(" Es menor el ",num3)
---
+++ Ejercicios_Control_Flujo/while/Ejercicio1/A2While1.py
@@ -0,0 +1,8 @@
+mod = False
+while mod == False:
+    si_teclean = input("Teclee algun valor distinto a enter: ")
+    print("Valor booleano ",bool(si_teclean))
+    if si_teclean:
+        
+    else:
+        print("Por favor tecleen")
